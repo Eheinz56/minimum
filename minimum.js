12f6f39b4c5a9d0083906026fbdfb74924502bfe
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-01-25 20:22:10 -0500

    Created and tested minimum.js Eric Heinzman CMP237 1/25/15

--- minimum.js
+++ minimum.js
@@ -1,1 +1,15 @@
-test
+//Eric Heinzman
+//CMP 237
+//1/25/15
+
+var min = function(x, y) {
+	return (x <= y) ? x : y;
+
+};
+
+console.log(min(0, 10));
+// > 0
+
+console.log(min(0, -10));
+// > -10
+
